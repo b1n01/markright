c5a1a642326833ad061cd0f5eeffb76c83c5930f
b1n01 <chielorenz@gmail.com>
2022-11-08 23:53:08 +0100

    Update tests

--- tests.js
+++ tests.js
@@ -2,74 +2,100 @@ const nearley = require("nearley");
 const { exec } = require("child_process");
 
 const tests = [
-  //----------
   // Paragraph
-  //----------
-
   {
     markright: "foo bar",
     html: "<p>foo bar</p>",
-    description: "Paragraph",
-  },
-  {
-    markright: "\nfoo\nbar\n",
-    html: "<p>foo bar</p>",
-    description: "Paragraph on multiple lines",
+    description: "A paragraph",
   },
   {
     markright: "   foo   bar   ",
     html: "<p>foo bar</p>",
-    description: "Paragraph spacing",
+    description: "A paragraph with spacing",
+  },
+  {
+    markright: "\nfoo\nbar\n",
+    html: "<p>foo bar</p>",
+    description: "A paragraph on multiple lines",
   },
   {
     markright: "   \n   foo   \n   bar   \n   ",
     html: "<p>foo bar</p>",
-    description: "Paragraph spacing on multiple lines",
+    description: "A paragraph on multiple lines with spacing",
   },
   {
-    markright: "foo\n\nbar",
-    html: "<p>foo</p><p>bar</p>",
-    description: "Multiple paragraph",
+    markright: "foo\n\nbar\n\n\nbaz",
+    html: "<p>foo</p><p>bar</p><p>baz</p>",
+    description: "Multiple paragraphs",
   },
   {
-    markright: "   foo   \n\n   bar   ",
-    html: "<p>foo</p><p>bar</p>",
-    description: "Spacing on multiple paragraphs",
-  },
-  {
-    markright: "foo\n\n\nbar",
-    html: "<p>foo</p><p>bar</p>",
-    description: "Multiple line breaks",
+    markright: "   foo   \n   \n   bar   \n   \n   \n   baz",
+    html: "<p>foo</p><p>bar</p><p>baz</p>",
+    description: "Multiple paragraphs with spacing",
   },
 
-  //-------
+  // H1
+  {
+    markright: "#foo bar",
+    html: "<h1>foo bar</h1>",
+    description: "An H1",
+  },
+  {
+    markright: "   #   foo   bar   ",
+    html: "<h1>foo bar</h1>",
+    description: "An h1 with spacing",
+  },
+  {
+    markright: "\n#\nfoo\nbar\n",
+    html: "<h1>foo bar</h1>",
+    description: "An H1 on multiple lines",
+  },
+  {
+    markright: "   \n   #   \n   foo   \n   bar   \n   ",
+    html: "<h1>foo bar</h1>",
+    description: "An H1 on multiple lines with spacing",
+  },
+  {
+    markright: "#foo\n\n#bar\n\n\n#baz",
+    html: "<h1>foo</h1><h1>bar</h1><h1>baz</h1>",
+    description: "Multiple h1s",
+  },
+  {
+    markright: "   #   foo   \n   \n   #   bar   \n   \n   \n   #   baz", 
+    html: "<h1>foo</h1><h1>bar</h1><h1>baz</h1>",
+    description: "Multiple h1s with spacing",
+  },
+
   // Strong
-  //-------
-
   {
-    markright: "[s:foo]",
-    html: "<p><strong>foo</strong></p>",
-    description: "Strong",
+    markright: "[s:foo bar]",
+    html: "<p><strong>foo bar</strong></p>",
+    description: "A strong",
   },
   {
-    markright: "[s:foo][s:bar]",
+    markright: "   [s:   foo   ]   [s:   bar   ]   ",
     html: "<p><strong>foo</strong><strong>bar</strong></p>",
-    description: "Multiple strong",
+    description: "A strong  with spacing",
   },
   {
     markright: "\n[s:\nfoo\nbar\n]\n",
     html: "<p><strong>foo bar</strong></p>",
-    description: "Strong on multiple lines",
+    description: "A strong on multiple lines",
   },
   {
-    markright: "   [s:   foo   bar   ]   [s:baz]",
-    html: "<p><strong>foo bar</strong><strong>baz qux</strong></p>",
-    description: "Spacing on multiple strong",
+    markright: "   \n   [s:   \n   foo   \n   bar   \n   ]   \n   ",
+    html: "<p><strong>foo bar</strong></p>",
+    description: "A strong on multiple lines with spacing",
   },
   {
-    markright: "[s:foo   bar   ]   [s:baz]",
-    html: "<p><strong>foo bar</strong><strong>baz qux</strong></p>",
-    description: "Spacing on multiple strong",
+    markright: "[s:foo]\n\n[s:bar]\n\n\n[s:baz]",
+    html: "<p><strong>foo</strong></p><p><strong>bar</strong></p><p><strong>baz</strong></p>",
+    description: "Multiple strong",
+  },
+  {
+    markright: "   [s:   foo   ]   \n   \n   [s:   bar   ]   \n   \n   \n   [s:   baz  ]   ",
+    html: "<p><strong>foo</strong></p><p><strong>bar</strong></p><p><strong>baz</strong></p>",
+    description: "Multiple strong with spacing",
   },
 ];
 
